601040e120aa0d335f0d0a4f3acdb4d0a8a2f365
Farsen976 <farsen.abdallahcombo@ynov.com>
2020-05-09 18:24:56 +0200

    feat (client): add maintenance page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Maintenance/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Maintenance from './components/Maintenance.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Maintenance
   }
 }
 </script>
@@ -22,7 +21,10 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #1b1b1b;
   margin-top: 60px;
+}
+html {
+    background-color: #f8c734;
 }
 </style>
---
+++ client/src/components/Maintenance.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="maintenance">
+<article>
+    <h1>Page under construction !</h1>
+    <div>
+        <p>We are currently working on the app. Follow <a href="https://twitter.com/thr3ad_up">@thr3ad_up</a> on Twitter to stay up to date.</p>
+        <p>&mdash; Thread's Team</p>
+    </div>
+</article>  
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Maintenance',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
